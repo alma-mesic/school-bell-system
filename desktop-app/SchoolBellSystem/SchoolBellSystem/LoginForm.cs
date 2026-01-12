eb2cb8cda5de61972adca1b339a087661d509918
Alma Mesic <almamesic40@gmail.com>
2026-01-12 17:43:35 +0100

    Windows App - Login Form

---
+++ desktop-app/SchoolBellSystem/SchoolBellSystem/LoginForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolBellSystem
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string adminUser = "admin";
+            string adminPass = "1234"; // kasnije ovo mozemo spremiti u config ili enkriptovati
+
+            if (textBox1.Text == adminUser && textBox2.Text == adminPass)
+            {
+                Form1 mainForm = new Form1();
+                mainForm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Pogrešan username ili lozinka!", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
