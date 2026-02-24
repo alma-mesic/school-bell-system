5ce5e6571a17599e7a91c1f0ccb8cf6bfe92f6a9
Alma Mesic <almamesic40@gmail.com>
2026-02-24 18:13:14 +0100

    Obavjestenja done

--- website/script.js
+++ website/script.js
@@ -7,8 +7,7 @@ if (burger) {
     }
 }
 
-
-
+let events = [];
 
 /********DIVS - main**********/
 
@@ -370,3 +369,58 @@ function setupColorPreview(inputId, previewId) {
 setupColorPreview('letter_color_change', 'letter_color_preview');
 setupColorPreview('marix_time_color_change', 'marix_time_color_preview');
 setupColorPreview('matrix_letter_color_change', 'matrix_letter_color_preview');
+
+/********************OBAVJEŠTENJA**********************/
+
+function addEvent() {
+    const name = document.getElementById("eventName").value;
+    const datetime = document.getElementById("datetime").value;
+
+    if (!name || !datetime) {
+        alert("Popuni sva polja!");
+        return;
+    }
+
+    const event = {
+        name: name,
+        time: new Date(datetime)
+    };
+
+    events.push(event);
+
+    sortEvents();
+    renderList();
+
+    document.getElementById("eventName").value = "";
+}
+
+function deleteEvent() {
+    const list = document.getElementById("eventList");
+    const index = list.selectedIndex;
+
+    if (index >= 0) {
+        events.splice(index, 1);
+        renderList();
+    }
+}
+
+function sortEvents() {
+    events.sort((a, b) => a.time - b.time);
+}
+
+function renderList() {
+    const list = document.getElementById("eventList");
+    list.innerHTML = "";
+
+    events.forEach(event => {
+        const option = document.createElement("option");
+        option.text = `${event.time.toLocaleString()} - ${event.name}`;
+        list.add(option);
+    });
+}
+
+setInterval(() => {
+    const now = new Date();
+    events = events.filter(event => event.time > now);
+    renderList();
+}, 60000); 
