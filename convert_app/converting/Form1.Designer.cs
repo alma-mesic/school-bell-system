4519845d9224a992c64ff9d6beac69ccfa9c679b
Lamija1518 <lamijamujkic1@gmail.com>
2026-04-06 19:11:38 +0200

    convert_app base

---
+++ convert_app/converting/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace converting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.select = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.convert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // select
+            // 
+            this.select.Location = new System.Drawing.Point(55, 43);
+            this.select.Name = "select";
+            this.select.Size = new System.Drawing.Size(152, 39);
+            this.select.TabIndex = 1;
+            this.select.Text = "Odaberi dokument";
+            this.select.UseVisualStyleBackColor = true;
+            this.select.Click += new System.EventHandler(this.select_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // convert
+            // 
+            this.convert.Location = new System.Drawing.Point(55, 140);
+            this.convert.Name = "convert";
+            this.convert.Size = new System.Drawing.Size(152, 39);
+            this.convert.TabIndex = 3;
+            this.convert.Text = "Konvertuj u json";
+            this.convert.UseVisualStyleBackColor = true;
+            this.convert.Click += new System.EventHandler(this.convert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 213);
+            this.Controls.Add(this.convert);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.select);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button select;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button convert;
+    }
+}
+
---
+++ convert_app/converting/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Tesseract;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace converting
+{ 
+    public partial class Form1 : Form
+    {
+        public class DanRaspored
+        {
+            public string date { get; set; }
+            public Dictionary<string, string> schedule { get; set; } = new Dictionary<string, string>();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void select_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void convert_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Molim vas prvo odaberite PDF fajl.");
+                return;
+            }
+
+            try
+            {
+                Spire.Pdf.PdfDocument doc = new Spire.Pdf.PdfDocument();
+                doc.LoadFromFile(textBox1.Text);
+                Bitmap bmp = (Bitmap)doc.SaveAsImage(0);
+                doc.Close();
+
+                var finalniRaspored = new Dictionary<string, DanRaspored>();
+
+                string[] naziviDana = { "Ponedjeljak", "Utorak", "Srijeda", "Četvrtak", "Petak" };
+                string[] satnice = {
+                    "07:00 do 08:00", "08:00 do 09:00", "09:00 do 10:00", "10:00 do 11:00",
+                    "11:00 do 12:00", "12:00 do 13:00", "13:00 do 14:00", "14:00 do 15:00",
+                    "15:00 do 16:00", "16:00 do 17:00", "17:00 do 18:00", "18:00 do 19:00"
+                };
+
+                int startX = 150;
+                int startY = 250;
+                int sirinaCelije = 120;
+                int visinaCelije = 35;
+
+                using (var engine = new TesseractEngine(@"./tessdata", "bos", EngineMode.Default))
+                {
+                    for (int kolona = 0; kolona < 5; kolona++)
+                    {
+                        string trenutniDan = naziviDana[kolona];
+
+                        if (!finalniRaspored.ContainsKey(trenutniDan))
+                        {
+                            finalniRaspored[trenutniDan] = new DanRaspored
+                            {
+                                date = DateTime.Now.Date.AddDays(kolona).ToString("yyyy-MM-dd"),
+                                schedule = new Dictionary<string, string>()
+                            };
+                        }
+
+                        for (int red = 0; red < 12; red++)
+                        {
+                            Rectangle rect = new Rectangle(
+                                startX + (kolona * sirinaCelije),
+                                startY + (red * visinaCelije),
+                                sirinaCelije,
+                                visinaCelije
+                            );
+
+                            using (Bitmap celijaBmp = bmp.Clone(rect, bmp.PixelFormat))
+                            using (var img = PixConverter.ToPix(celijaBmp))
+                            using (var page = engine.Process(img, PageSegMode.SingleLine))
+                            {
+                                string procitanoIme = page.GetText().Trim();
+
+                                if (!string.IsNullOrWhiteSpace(procitanoIme))
+                                {
+                                    procitanoIme = System.Text.RegularExpressions.Regex.Replace(procitanoIme, @"[|._\-]", "");
+                                    finalniRaspored[trenutniDan].schedule[satnice[red]] = procitanoIme;
+                                }
+                                else
+                                {
+                                    finalniRaspored[trenutniDan].schedule[satnice[red]] = "";
+                                }
+                            }
+                        }
+                    }
+                }
+
+                File.WriteAllText("raspored.json", JsonConvert.SerializeObject(finalniRaspored, Formatting.Indented));
+                MessageBox.Show("Konvertovanje gotovo!");
+            }
+            catch (Exception ex) { MessageBox.Show("Greška: " + ex.Message); }
+        }
+    }
+}
+
+
+
